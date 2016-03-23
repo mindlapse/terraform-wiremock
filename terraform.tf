2170c6cccaf77686740b6cdafc6199026ce57601
Dave MacDonald <mindlapse@gmail.com>
2016-03-23 15:55:15 -0400

    Removed key_file

--- terraform.tf
+++ terraform.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "wiremock" {
   instance_type   = "${var.instance_size}"
   security_groups = ["ssh_group", "tls_group"]
   key_name        = "${var.aws_ssh_key_name}"
-  key_file        = "${var.aws_ssh_key_path}"
+#  key_file        = "${var.aws_ssh_key_path}"
 
   provisioner "local-exec" {
     command = "ansible-playbook ${aws_instance.wiremock.public_ip} ./ansible/playbook.yml --extra-vars 'git_repo=${repo} git_branch=${branch}'"
