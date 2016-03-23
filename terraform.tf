3618ce0fd4278b7a02e5e508dd57ff285e6f0162
Dave MacDonald <mindlapse@gmail.com>
2016-03-23 15:49:35 -0400

    Corrected eip var

--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,15 @@
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region = "${var.region}"
+}
 
 resource "aws_instance" "wiremock" {
-  ami = "${lookup(var.amis, var.region)}"
-  instance_type = "${var.instance_size}"
+  ami             = "${lookup(var.amis, var.region)}"
+  instance_type   = "${var.instance_size}"
   security_groups = ["ssh_group", "tls_group"]
-  key_name = "${var.ssh_key_name}"
-  key_file = "${var.ssh_key_path}"
+  key_name        = "${var.aws_ssh_key_name}"
+  key_file        = "${var.aws_ssh_key_path}"
 
   provisioner "local-exec" {
     command = "ansible-playbook ${aws_instance.wiremock.public_ip} ./ansible/playbook.yml --extra-vars 'git_repo=${repo} git_branch=${branch}'"
--- variables.tf
+++ variables.tf
@@ -2,10 +2,10 @@ variable "repo" {}            // The github repo where the wiremock samples can 
                               // The repo should have a /samples folder with .json files containing
                               // the samples
 
-variable "secret_key" { }     // Your AWS secret key
-variable "access_key" { }     // Your AWS access key
-variable "ssh_key_name" { }   // The name of your ssh key pair from AWS  (e.g. 'masterkey')
-variable "ssh_key_path" { }   // The path to your ssh key pair .pem file (e.g. '/vagrant/masterkey.pem')
+variable "aws_secret_key" { } // Your AWS secret key
+variable "aws_access_key" { } // Your AWS access key
+variable "aws_ssh_key_name" { }   // The name of your ssh key pair from AWS  (e.g. 'masterkey')
+variable "aws_ssh_key_path" { }   // The path to your ssh key pair .pem file (e.g. '/vagrant/masterkey.pem')
 
 variable "instance_size" {
   default = "t2.nano"
