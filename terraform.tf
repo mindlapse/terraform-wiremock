2bc821052b1bbaec6a6e0c26319320970d7a80cc
Dave MacDonald <mindlapse@gmail.com>
2016-03-23 16:41:30 -0400

    Updates

--- terraform.tf
+++ terraform.tf
@@ -8,11 +8,11 @@ resource "aws_instance" "wiremock" {
   ami             = "${lookup(var.amis, var.region)}"
   instance_type   = "${var.instance_size}"
   security_groups = ["ssh_group", "tls_group"]
-  key_name        = "${var.aws_ssh_key_name}"
+#  key_name        = "${var.aws_ssh_key_name}"
 #  key_file        = "${var.aws_ssh_key_path}"
 
   provisioner "local-exec" {
-    command = "ansible-playbook ${aws_instance.wiremock.public_ip} ./ansible/playbook.yml --extra-vars 'git_repo=${repo} git_branch=${branch}'"
+    command = "ansible-playbook ${aws_instance.wiremock.public_ip} ./ansible/playbook.yml --private-key=${var.aws_ssh_key_path} --extra-vars 'git_repo=${repo} git_branch=${branch}'"
   }
 }
 
