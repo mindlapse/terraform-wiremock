dfb78d707ee3537697a3259195899157844f1563
Dave MacDonald <mindlapse@gmail.com>
2016-03-24 23:19:33 -0400

    Finally works

--- terraform.tf
+++ terraform.tf
@@ -7,14 +7,22 @@ provider "aws" {
 resource "aws_instance" "wiremock" {
   ami             = "${lookup(var.amis, var.region)}"
   instance_type   = "${var.instance_size}"
-  security_groups = ["ssh_group", "web_group"]
+  security_groups = ["${aws_security_group.ssh_group.name}", "${aws_security_group.web_group.name}"]
   key_name        = "${var.aws_ssh_key_name}"
 
-  provisioner "local-exec" {
-    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook ./ansible/playbook.yml --private-key=${var.aws_ssh_key_path} --extra-vars 'git_repo=${repo} git_branch=${branch}' -i ${aws_instance.wiremock.public_ip}, -u ${var.instance_user} -vvvv"
-  }
 }
 
 resource "aws_eip" "ip" {
   instance = "${aws_instance.wiremock.id}"
+
+  connection {
+    host = "${aws_eip.ip.public_ip}"
+    user = "${var.instance_user}"
+    private_key = "${file("${var.aws_ssh_key_path}")}"
+  }
+
+  provisioner "local-exec" {
+    # The sleep is to wait for sshd to come online before attempting to provision (there must be a better way)
+    command = "sleep 180 && ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook ${path.module}/ansible/playbook.yml --private-key=${var.aws_ssh_key_path} --extra-vars 'git_repo=${var.repo} git_branch=${var.branch}' -i ${aws_eip.ip.public_ip}, -u ${var.instance_user} -vvvv"
+  }
 }
